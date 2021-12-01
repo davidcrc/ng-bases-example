b37b1d75d671d1291d2288381a06dbef37037e9e
david <david_drcr@hotmail.com>
2021-12-01 18:20:58 -0500

    5-11. Tarea con inputs y módulos

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -16,13 +16,4 @@ export class MainPageComponent {
     nombre: '',
     poder: 0,
   };
-
-  agregar() {
-    if (this.nuevo.nombre.trim().length === 0) return;
-
-    console.log(this.nuevo);
-
-    this.personajes.push(this.nuevo);
-    this.nuevo = { nombre: '', poder: 0 };
-  }
 }
