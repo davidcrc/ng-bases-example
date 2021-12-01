27e44cf4e8c99ff3ec4306c921bad18221834d83
david <david_drcr@hotmail.com>
2021-12-01 14:46:52 -0500

    4-15. Crear componente de forma automática

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: [],
+})
+export class ListadoComponent {}
