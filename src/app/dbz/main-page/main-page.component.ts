9479fe1c35a79eda32be36ccfd5d2acff715ecae
david <david_drcr@hotmail.com>
2021-12-01 16:48:39 -0500

    5-8. Mostrar listado de personajes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -11,12 +11,22 @@ interface Personaje {
   styleUrls: [],
 })
 export class MainPageComponent {
+  personajes: Personaje[] = [
+    { nombre: 'Goku', poder: 20000 },
+    { nombre: 'Vegete', poder: 2000 },
+  ];
+
   nuevo: Personaje = {
-    nombre: 'Trunks',
-    poder: 12000,
+    nombre: '',
+    poder: 0,
   };
 
   agregar() {
+    if (this.nuevo.nombre.trim().length === 0) return;
+
     console.log(this.nuevo);
+
+    this.personajes.push(this.nuevo);
+    this.nuevo = { nombre: '', poder: 0 };
   }
 }
