1c64865d84a3f5dab672b436391cbc949a13db00
david <david_drcr@hotmail.com>
2021-12-02 17:14:49 -0500

    5-14. Servicios

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,6 +8,8 @@ import { Personaje } from '../interfaces/dbz.interface';
   styleUrls: [],
 })
 export class MainPageComponent {
+  constructor(private dbzService: DbzService) {}
+
   personajes: Personaje[] = [
     { nombre: 'Goku', poder: 20000 },
     { nombre: 'Vegete', poder: 2000 },
